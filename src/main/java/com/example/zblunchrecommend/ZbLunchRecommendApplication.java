ab16705471971d49a9fb94f63746fffac6da6724
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2024-09-19 16:35:48 +0900

    init

---
+++ src/main/java/com/example/zblunchrecommend/ZbLunchRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zblunchrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbLunchRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbLunchRecommendApplication.class, args);
+    }
+
+}
